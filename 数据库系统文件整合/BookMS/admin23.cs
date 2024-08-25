0249b9fe80b9311680054c5cd066325c303e676d
huangannan <163302506+huangannan@users.noreply.github.com>
2024-08-25 18:46:23 +0800

    Add files via upload

---
+++ 数据库系统文件整合/BookMS/admin23.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookMS
+{
+    public partial class admin23 : Form
+    {
+        string ID = "";
+        public admin23()
+        {
+            InitializeComponent();
+        }
+        //构造方法，将书的那几个参数直接传进来
+        public admin23(string id, string name, string author, string press, string number)
+        {
+            InitializeComponent();
+            ID=textBox4.Text = id;
+            textBox2.Text = name;
+            textBox1.Text = author;
+            textBox3.Text = press;
+            textBox5.Text = number;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql = $"update t_book set id='{textBox4.Text}'," +
+                $"[name]='{textBox2.Text}',author='{textBox1.Text}'," +
+                $"press='{textBox3.Text}',number={textBox5.Text} where id='{ID}'";
+            Dao dao = new Dao();
+            if (dao.Execute(sql)>0)
+            {
+                MessageBox.Show("修改成功");
+                this.Close();
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 数据库系统文件整合/BookMS/login.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace BookMS
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                Login();
+            }
+            else
+            {
+                MessageBox.Show("输入有空项，请重新输入");
+            }
+            
+        }
+
+        //登陆方法
+        public void Login()
+        {
+            //用户
+            if (radioButtonUser.Checked == true)
+            {
+                Dao dao = new Dao();
+                string sql = "select* from t_user where id='"+textBox1.Text+"'and psw='"+textBox2.Text+"'";
+               
+                IDataReader dc = dao.read(sql);
+                if (dc.Read())
+                {
+                    Data.UID = dc["id"].ToString();//存放登录用户的id
+                    Data.UID = dc["name"].ToString();
+                    MessageBox.Show("登陆成功");
+                    user1 user = new user1();//窗体实例化
+                    this.Hide();
+                    user.ShowDialog();
+                    this.Show();
+                }
+                else
+                {
+                    MessageBox.Show("登陆失败");
+                    
+                }
+                dao.DaoClose();
+            }
+            //管理员
+            if (radioButtonAdmin.Checked == true)
+            {
+                Dao dao = new Dao();
+                string sql = "select* from t_admin where id='" + textBox1.Text + "'and psw='" + textBox2.Text + "'";
+
+                IDataReader dc = dao.read(sql);
+                if (dc.Read())
+                {
+                    MessageBox.Show("登陆成功");
+                    admin1 admin = new admin1();
+                    this.Hide();
+                    admin.ShowDialog();
+                    this.Show();
+                }
+                else
+                {
+                    MessageBox.Show("登陆失败");
+                    
+                }
+                dao.DaoClose();
+            }
+            MessageBox.Show("登陆失败");
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
